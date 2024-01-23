57bb5b012e57c8441de73d0c2d2f390b4ed56d69
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-01-23 13:44:02 -0800

    feat: adding other routes back into application

--- frontend/hydro_alerting/src/app/app.routes.ts
+++ frontend/hydro_alerting/src/app/app.routes.ts
@@ -1,16 +1,14 @@
-import { RouterModule, Routes } from '@angular/router';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule  } from '@angular/router';
 import { BasinListComponent } from './basin-list/basin-list.component';
 import { AlertListComponent } from './alert-list/alert-list.component';
 import { HomeComponent } from './home/home.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
     // { path: '', component: AlertListComponent },
-    // { path: 'home', component: HomeComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'alertlist', component: AlertListComponent },
     { path: 'basins', component: BasinListComponent },
     { path: 'unauthorized', component: UnauthorizedComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '', redirectTo: 'home'},
 ];
