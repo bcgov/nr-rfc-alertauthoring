4262c971832674367b3c599f657afb9b48936dd3
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-02-09 14:54:06 -0800

    feat: added quill editor components for paragraph fields

--- frontend/hydro_alerting/src/app/alerts/view-alert/view-alert.component.ts
+++ frontend/hydro_alerting/src/app/alerts/view-alert/view-alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { AlertService } from '../alert.service';
 import { switchMap, Observable, of } from 'rxjs';
@@ -7,25 +7,52 @@ import { ReactiveFormsModule, FormsModule, FormBuilder, FormGroup } from '@angul
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import Quill from 'quill'
+import { MatQuillModule } from '../../mat-quill/mat-quill-module'
+import { EditorChangeContent, EditorChangeSelection, QuillEditorComponent } from 'ngx-quill'
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import utc from 'dayjs/plugin/utc';
+
+
 
 
 @Component({
   selector: 'app-view-alert',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule ],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    QuillEditorComponent,
+    MatQuillModule,
+    MatFormFieldModule],
   templateUrl: './view-alert.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './view-alert.component.css',
 })
 export class ViewAlertComponent {
-typeof(arg0: string|null) {
-throw new Error('Method not implemented.');
-}
+  typeof(arg0: string | null) {
+    throw new Error('Method not implemented.');
+  }
   alert_id: number | undefined;
   alert!: Observable<Alert>;
   editable: string | null = null;//"readonly";
   form_disabled: string | null = 'disabled';
   single_alert_form: FormGroup;
+
+  meteorological_data_contents: string = "";
+  hydrological_data_contents: string = "";
+
+  // quill component properties
+  blurred = false;
+  focused = false;
+  quillReadonly=true;
+
 
   constructor(private route: ActivatedRoute, private alertService: AlertService, private formBuilder: FormBuilder) {
     this.single_alert_form = this.formBuilder.group({
@@ -46,11 +73,13 @@ throw new Error('Method not implemented.');
         this.alert_id = params['id'];
         return this.alertService.getAlert(params['id']);
       }))
-      .subscribe((alert) => {
+      .subscribe((alert: Alert) => {
         console.log('alert: ' + JSON.stringify(alert));
+        this.meteorological_data_contents = alert.alert_meteorological_conditions;
+        this.hydrological_data_contents = alert.alert_hydro_conditions;
         this.alert = of(alert);
       });
-      this.single_alert_form.disable();
+    this.single_alert_form.disable();
   }
 
   onEnableEditing() {
@@ -61,16 +90,64 @@ throw new Error('Method not implemented.');
     }
     if (this.form_disabled === 'disabled') {
       this.form_disabled = null;
+      this.quillReadonly = false;
     } else {
       this.form_disabled = 'disabled';
+      this.quillReadonly = true;
     }
   }
 
+  /**
+   * 
+   * @param date input datestring in the format of "YYYY-MM-DDTHH:MM:SS.SSSSSS"
+   * @returns date string in the format of "ddd MMM D, YYYY HH:MM:ss"
+   */
   format_date(date: string) {
-    // converts the date string to a date object, then spits back out 
-    // according to the format requested
     console.log("date: " + date);
-    return "";
-  } 
+    dayjs.extend(customParseFormat);
+    let date_djs = dayjs(date, "YYYY-MM-DDTHH:MM:SS.SSSSSS");
+    console.log("date_djs: " + date_djs);
+    // TODO: all dates are in UTC atm... long term likely want to convert to 
+    //       local time zone
+    let date_str = date_djs.format('HH:MM:ss ddd MMM D, YYYY');
+    return date_str;
+  }
+
+
+// -------------------------- Quill components --------------------------
+
+
+  changedEditor(event: EditorChangeContent | EditorChangeSelection | any) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-change', event)
+  }
+
+  focus($event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('focus', $event)
+    this.focused = true
+    this.blurred = false
+  }
+  nativeFocus($event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('native-focus', $event)
+  }
+
+  blur($event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('blur', $event)
+    this.focused = false
+    this.blurred = true
+  }
+  nativeBlur($event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('native-blur', $event)
+  }
+
+  created(editor: any) {
+    const range = editor.getSelection(true)
+    
+    // editor.insertText(0, this.meteorological_data_contents)
+  }
 
 }
--- frontend/hydro_alerting/src/app/app.config.ts
+++ frontend/hydro_alerting/src/app/app.config.ts
@@ -8,6 +8,8 @@ import { provideAuth } from 'angular-auth-oidc-client';
 import { routes } from './app.routes';
 import { authCodeFlowConfig } from './sso.config';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideQuillConfig } from 'ngx-quill/config';
+
 
 
 
@@ -30,5 +32,12 @@ export const appConfig: ApplicationConfig = {
         authCodeFlowConfig
     }),
     provideAnimations(),
+    provideQuillConfig({
+        customOptions: [{
+        import: 'formats/font',
+        whitelist: ['mirza', 'roboto', 'aref', 'serif', 'sansserif', 'monospace']
+      }]
+    })
+
   ]
 };
