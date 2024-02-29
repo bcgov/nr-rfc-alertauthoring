387ddcd927def04fab183ffbcc5994073b51a200
Guy Lafleur <kevin.netherton@gov.bc.ca>
2024-02-29 09:57:51 -0800

    feat: 36 create page (#37)

--- frontend/hydro_alerting/src/app/alerts/create-alert/create-alert.component.ts
+++ frontend/hydro_alerting/src/app/alerts/create-alert/create-alert.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+
+import Quill from 'quill'
+import { MatQuillModule } from '../../mat-quill/mat-quill-module'
+import { MatQuill } from '../../mat-quill/mat-quill'
+
+import { EditorChangeContent, EditorChangeSelection, QuillEditorComponent } from 'ngx-quill'
+
+
+
+
+
 
 @Component({
   selector: 'app-create-alert',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, 
+    CommonModule, 
+    ReactiveFormsModule, 
+    MatInputModule, 
+    MatButton, 
+    MatSelectModule,
+    QuillEditorComponent,
+    MatQuillModule],
   templateUrl: './create-alert.component.html',
-  styleUrl: './create-alert.component.css'
+  styleUrl: './create-alert.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class CreateAlertComponent {
+export class CreateAlertComponent implements OnInit {
+
+  create_alert_form: FormGroup;
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private router: Router) {
+    this.create_alert_form = this.formBuilder.group({
+      alert_description: ["",],
+      alert_status: ["",],
+      meteorologicalDataEditor: [""],
+      hydrologicalDataEditor: [""],
+    });
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+  onSubmitCreate() {
+    // Todo: push this data to the api, the api should return the new alert id
+    // that has been generated, then the route will navigate to the view alert
+    console.log('Create Alert Form Submitted');
+    console.log(`form data: ${JSON.stringify(this.create_alert_form.value)}`);
+    // hard coding the redirect atm
+    this.router.navigate(['/alert', '1']);
+  }
+
+  resetForm() {
+    console.log("reset form")
+    this.create_alert_form.get('alert_description')!.reset();
+    this.create_alert_form.get('meteorologicalDataEditor')!.reset();
+    this.create_alert_form.get('hydrologicalDataEditor')!.reset();
+    this.create_alert_form.get('alert_status')!.reset();
+  }
 
 }
