d14e6426ab2c41d451bb3556e3b522c388be229a
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-04-17 16:09:11 -0700

    feat: apply bcgov design

--- frontend/hydro_alerting/src/app/app.component.ts
+++ frontend/hydro_alerting/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HeaderComponent} from './header/header.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // import { Observable } from 'rxjs';
@@ -18,12 +19,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     standalone: true,
     template: 
     `
+    <app-header></app-header>
     <app-navbar></app-navbar>
     <br><br>
     <router-outlet></router-outlet>
    `,
     styles: ['h1 { color: blue; }'],
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, HeaderComponent]
 })
 export class AppComponent  implements OnInit{
   title = 'Hydrological Alert Authoring System (HAAS)';
