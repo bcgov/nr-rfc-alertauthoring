3ae90401690a1675bde5f4c5891992b0e33c30d7
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-05-03 15:15:26 -0700

    feat: adding tests for the cap end point

--- backend/test/api_tests/test_api_caps.py
+++ backend/test/api_tests/test_api_caps.py
@@ -2,6 +2,7 @@ import logging
 import pprint
 
 import fastapi.testclient
+import src.v1.models.alerts as alert_models
 from sqlmodel import Session, select
 from src.core.config import Configuration
 
@@ -19,9 +20,35 @@ def test_get_caps(db_with_alert: Session, test_client_with_alert_and_cap, alert_
     LOGGER.debug(f"response: {response}")
     LOGGER.debug(f"responsedata : {response.json()}")
 
-    data = response.json()
-    data_formatted = pprint.pformat(data, indent=4)
+    cap_dict = response.json()
+    data_formatted = pprint.pformat(cap_dict, indent=4)
 
     LOGGER.debug(f"data: \n{data_formatted}\n")
 
+    # verify that the caps where created properly
+    #  first get the alert for the cap
+    alert_query = select(alert_models.Alerts).where(alert_models.Alerts.alert_description == alert_dict["alert_description"])
+    alert_records = session.exec(alert_query).fetchall()
+    alert_record = alert_records[-1] # get the last record
+    alert_id_to_verify = alert_record.alert_id
+    LOGGER.debug(f"verify the alert id: {alert_id_to_verify}")
 
+    # now verify that the alert levels / basins in the alert record correspond 
+    # to the cap records
+    
+    # verify the alert levels
+    # go through the alert and organize the basins by alert levels
+    alert_lvl_basins_dict = {}
+    for alert_link in alert_record.alert_links:
+        if alert_link.alert_level.alert_level not in alert_lvl_basins_dict:
+            alert_lvl_basins_dict[alert_link.alert_level.alert_level] = []
+        alert_lvl_basins_dict[alert_link.alert_level.alert_level].append(alert_link.basin.basin_name)
+
+    # now iterate over the caps and verify the cap data is in the alert
+    for cap in cap_dict:
+        assert cap['alert_id'] == alert_id_to_verify
+        # verify the alert level is in the alert
+        assert cap['alert_level']['alert_level'] in alert_lvl_basins_dict
+        # verify the basins
+        for event_area in cap['event_areas']:
+            assert event_area['cap_area_basin']['basin_name'] in alert_lvl_basins_dict[cap['alert_level']['alert_level']]
