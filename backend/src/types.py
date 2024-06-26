f6e736c1f66ab0511dece0625e15edb57adf19ec
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-06-26 13:41:22 -0700

    docs: add comments to describe what the different typed dics are used for

--- backend/src/types.py
+++ backend/src/types.py
@@ -2,9 +2,10 @@ import typing
 
 from typing_extensions import TypedDict
 
+# The data model used for RFC alerts
 AlertAreaLevel = TypedDict("AlertAreaLevel", {"basin": str, "alert_level": int})
 
-
+# the data model used to describe data used to create caps
 AlertDataDict = typing.TypedDict(
     "AlertDataDict", {"alert_level": str, "basin_names": typing.List[str]}
 )
