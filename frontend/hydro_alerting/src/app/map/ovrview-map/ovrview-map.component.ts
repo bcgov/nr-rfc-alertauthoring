a0f584d739ad10159d3d1d3b2c44e4c85dd3be25
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-08-13 17:04:49 -0700

    fix: resolve styles not loading on app load sometimes

--- frontend/hydro_alerting/src/app/map/ovrview-map/ovrview-map.component.ts
+++ frontend/hydro_alerting/src/app/map/ovrview-map/ovrview-map.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, AfterViewInit, Input, ElementRef, ViewChild, Inject } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { Alert, AlertAreaLevels } from '../../types/alert';
 import { Map, map, tileLayer, popup, Control, DomUtil } from 'leaflet';
 import { Observable } from 'rxjs';
 import { MapUtil } from "../map_defaults";
 import {AlertCreate} from '../../types/alert';
 import { DOCUMENT } from '@angular/common'; 
+import { FeatureLayer } from 'esri-leaflet';
 
 
 @Component({
@@ -27,27 +28,19 @@ export class OvrviewMapComponent implements OnInit, AfterViewInit {
   public _map!: Map;
   basins: string[] = [];
   alert_levels: string[] = [];
+  basins_fl!: FeatureLayer;
 
   // # alertLevels
   ngAfterViewInit() {
     this.initMap();
   }
 
-  constructor(@Inject(DOCUMENT) document: Document) {
-    const mapElement = document.getElementById('map');
-    if (mapElement) {
-      mapElement.innerHTML = "";
-    }
-    this.mapElem = mapElement;
+  constructor() {
+    
   }
 
   ngOnInit(): void {
     console.log("Doing the subscription to the basin / alert levels")
-    // this.alertLevels.subscribe(val => {
-    //   /// getting the data from the observable
-    //   console.log(`value: ${JSON.stringify(val)}`);
-    // })
-    // console.log(`alert levels: ${JSON.stringify(this.alertLevels)}`);
     this.readBasinAlertLevels();
   }
 
@@ -60,10 +53,15 @@ export class OvrviewMapComponent implements OnInit, AfterViewInit {
           let alert: AlertCreate = alertCreate[i];
           if (alert.alert_links) {
             for (let j = 0; j < alert.alert_links.length; j++) {
+              console.log("got basin: " + alert.alert_links[j].basin.basin_name);
               this.basins.push(alert.alert_links[j].basin.basin_name);
               this.alert_levels.push(alert.alert_links[j].alert_level.alert_level);
             }
           }
+        }
+        if (this.basins_fl != undefined) {
+          // indicates the map has loaded, possibly after the alert levels have been loaded
+          this.basins_fl.setStyle(this.styleMap);
         }
     });
     }
@@ -75,33 +73,26 @@ export class OvrviewMapComponent implements OnInit, AfterViewInit {
     // for the overview map was erroring out with the error: 
     // "Map container is already initialized"  Manually setting the outerHTML
     // seems to resolve this issue.
-    this.readBasinAlertLevels();
+    //this.readBasinAlertLevels();
     let container: any = DomUtil.get("map");
   
     
     if (JSON.stringify(container) != JSON.stringify({})) {
       DomUtil.empty(container);
-      // this is what works
+      // this is what works to remove the map container from the DOM, when the 
+      // page gets a partial reload, without this code will eventually get a 
+      // "Map container is already initialized" error
       container.outerHTML = "";
       container.remove();
     }
-  
-    console.log("init map");
-    if(this._map != undefined || this._map != null){
-      console.log("removing map");
-      this._map.invalidateSize();
-      // this._map.remove();
-    }
 
-    // console.log(`map is: ${JSON.stringify(this._map)}`);
     this._map = this.mapUtil.getMapObj(this._map);
     let legend = this.mapUtil.defineLegend(this._map);
     legend.addTo(this._map);
-    let basins_fl = this.mapUtil.addBasins(this._map, this.styleMap)
-    basins_fl.addTo(this._map);
+    this.basins_fl = this.mapUtil.addBasins(this._map, this.styleMap)
+    this.basins_fl.addTo(this._map);
 
-    this.mapUtil.addBasinPopup(this._map, basins_fl, this.basin_lvl_lu_func);
-
+    this.mapUtil.addBasinPopup(this._map, this.basins_fl, this.basin_lvl_lu_func);
   }
 
   styleMap = (feature: any): any => {
